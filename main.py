d08c33d07e7b539143ea196e80ee8c376aefd20c
Lucas Andrade <Lucasavitorino1@gmail.com>
2024-10-03 08:59:06 -0300

    Added main code and folder Vendas

---
+++ main.py
@@ -0,0 +1,19 @@
+#Passo 0 - Entender o desafio
+#Passo 1 - Percorrer todos os arquivos da base de dados 
+
+
+#Passo 2 - Importar as bases de dados de vendas
+#Passo 3 - Tratar / compilar as bases de dados
+#Passo 4 - Calcular o produto mais vendido (em quantidade)
+#Passo 5 - Calcular o produto que mais faturou (em faturamento)
+#Passo 6 - Calcular a loja / cidade que mais vendeu (em faturamento) - criar um gráfico / dashboard
+
+import os
+
+archives_list = os.listdir(r"C:\Users\Lucas\OneDrive\Área de Trabalho\workspace-python\CursoPython\Vendas")
+print(archives_list)
+print("\n")
+for archive in archives_list:
+    print(archive)
+    
+    
